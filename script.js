79566e94310091b0c912c0e652eceb096fc78dd9
emines0 <emin.sefa1@gmail.com>
2023-01-17 17:36:37 +0100

    using interval, update time

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const  word = document.getElementById('word');
 const  text = document.getElementById('text');
 const  scoreEl = document.getElementById('score');
 const  timeEl = document.getElementById('time');
-const  endgameEl = document.getElementById('end-game');
+const  endgameEl = document.getElementById('end-game-container');
 const  settingsbtn = document.getElementById('settings-btn');
 const  settings = document.getElementById('settings');
 const  settingsForm = document.getElementById('settings-form');
@@ -59,6 +59,12 @@ let time = 10;
 
 // }
 
+// Focus on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000)
+
 // Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -76,7 +82,31 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
+// Update time
 
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if(time === 0 ) {
+    clearInterval(time);
+    //end game
+    gameOver();
+  }
+}
+
+// Game over, show end screen
+
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
+  
+}
 
 addWordToDOM();
 
@@ -90,5 +120,9 @@ text.addEventListener('input', e => {
 
       // Clear
       e.target.value = '';
+
+      time += 2;
+
+      updateTime();
     }
 });
